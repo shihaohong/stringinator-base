ce6e91877f0eff68987d2ef869ef26b210f5013a
Shi-Hao Hong <shihaohong94@gmail.com>
2017-05-07 19:08:50 -0700

    implemented last and indexOf functions as well as their Test Suites

--- test/underbar/indexOf.test.js
+++ test/underbar/indexOf.test.js
@@ -2,23 +2,23 @@ const _ = require('../../underbar');
 
 describe('indexOf()', () => {
   it('returns the index of a value at the beginning of an array', () => {
-    // Your code goes here
+    expect(_.indexOf(['a', 'b', 'c'], 'a')).toBe(0);
   });
 
   it('returns the index of a value at the end of an array', () => {
-    // Your code goes here
+    expect(_.indexOf(['a', 'b', 'c'], 'c')).toBe(2);
   });
 
   it('returns -1 for a missing value', () => {
-    // Your code goes here
+    expect(_.indexOf(['a', 'b', 'c'], 'd')).toBe(-1);
   });
 
   it('returns the first matching index when multiple matches in array', () => {
-    // Your code goes here
+    expect(_.indexOf(['c', 'b', 'a', 'a', 'a', 'a', 'a'], 'a')).toBe(2);
   });
 
   it('starts searching at the given offset', () => {
-    // Your code goes here
+    expect(_.indexOf(['a', 'b', 'c', 'a', 'a'], 'a', 2)).toBe(3);
   });
 
 });
--- test/underbar/last.test.js
+++ test/underbar/last.test.js
@@ -2,14 +2,15 @@ const _ = require('../../underbar');
 
 describe('last()', () => {
   it('returns the last element of an array', () => {
-    // Your code goes here
+    expect(_.last([1, 2, 3])).toBe(3);
+    
   });
 
   it('returns the last 2 elements of an array', () => {
-    // Your code goes here
+    expect(_.last(['a', 'b', 'c'], 2)).toEqual(['b', 'c']);
   });
 
   it('returns the whole array if you ask for more elements than it has', () => {
-    // Your code goes here
+    expect(_.last([1, 2, 3], 5)).toEqual([1, 2, 3]);
   });
 });
