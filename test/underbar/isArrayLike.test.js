e23d3c1e8c1ceb63a1d166bf88d93f404cff023c
Shi-Hao Hong <shihaohong94@gmail.com>
2017-05-07 19:13:28 -0700

    added isArrayLike() functionality and implemented its Test Suite

--- test/underbar/isArrayLike.test.js
+++ test/underbar/isArrayLike.test.js
@@ -2,14 +2,26 @@ const _ = require('../../underbar');
 
 describe('isArrayLike()', () => {
   it('returns true for an actual array', () => {
-    // Your code goes here
+    expect(_.isArrayLike([1, 2, 3])).toBe(true);
   });
 
   it('returns true for an array-like object', () => {
-    // Your code goes here
+    const obj = {
+      length: 3,
+      1: 'a',
+      2: 'b',
+      3: 'c'
+    }
+    
+    expect(_.isArrayLike(obj)).toBe(true);
   });
 
   it('returns false for a non-array-like object', () => {
-    // Your code goes here
+    const obj = {
+      'hello': 'its me',
+      'i was': 'wondering'
+    }
+
+    expect(_.isArrayLike(obj)).toBe(false);
   });
 });
