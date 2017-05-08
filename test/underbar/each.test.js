3218210cb4e26497e1821561a12b64b39b37cbd6
Shi-Hao Hong <shihaohong94@gmail.com>
2017-05-07 19:27:05 -0700

    each() functionality added and Test Suite was completed

--- test/underbar/each.test.js
+++ test/underbar/each.test.js
@@ -2,15 +2,48 @@ const _ = require('../../underbar');
 
 describe('each()', () => {
   it('iterates every element of an array, passing that element, its corresponding index, and the entire array to the callback', () => {
-    // Your code goes here
+    const arr = [1, 2, 3, 4, 5];
+    let count = 0;
+
+    _.each(arr, function(value, index, array) {
+      expect(value).toBe(array[index]);
+      count += 1;
+    })
+
+    expect(count).toBe(5);
   });
 
   it('iterates every element of an array-like object, passing that element, its corresponding index, and the entire array to the callback', () => {
-    // Your code goes here
+    const obj = {
+      length: 3,
+      1: 'a',
+      2: 'b',
+      3: 'c'
+    }
+    let count = 0;
+
+    _.each(obj, function(element, index, object){
+      expect(element).toBe(object[index]);
+      count++;
+    });
+
+    expect(count).toBe(3);
   });
 
   it('iterates every property of an object, passing the value, the corresponding key, and the entire object to the callback', () => {
-    // Your code goes here
+    const obj = {
+      'first name': 'Shi-Hao',
+      'last name': 'Hong',
+      'school': 'USC'
+    }
+    let count = 0;
+
+    _.each(obj, function(element, index, object){
+      expect(element).toBe(object[index]);
+      count++;
+    });
+
+    expect(count).toBe(3);
   });
 });
 
