18ba29cd4ca986988675d82cedc1705510fdd347
Shi-Hao Hong <shihaohong94@gmail.com>
2017-05-10 01:05:18 -0700

    added map functionality and map Test Suite

--- test/underbar/map.test.js
+++ test/underbar/map.test.js
@@ -2,6 +2,9 @@ const _ = require('../../underbar');
 
 describe('map()', () => {
   it('maps every numbers in an array of numbers to their square', () => {
-    // Your code goes here
+    const array = [1, 2, 3, 4, 5];
+    const mappedArr = _.map(array, (el) => el*el); 
+
+    expect(mappedArr).toEqual([1, 4, 9, 16, 25]);
   });
 });
