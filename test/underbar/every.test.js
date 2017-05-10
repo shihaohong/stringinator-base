9cc4cc2f416fd6963f8b5cfb3dab40ae30752b5a
Shi-Hao Hong <shihaohong94@gmail.com>
2017-05-10 01:53:09 -0700

    added every() functionality and completed its Test Suite

--- test/underbar/every.test.js
+++ test/underbar/every.test.js
@@ -3,15 +3,18 @@ const _ = require('../../underbar');
 describe('every()', () => {
   describe('processing an array of numbers', () => {
     it('will return true if no callback is supplied', () => {
-      // Your code goes here
+      const arr = [1, 2, 3];
+      expect(_.every(arr)).toBe(true);
     });
 
     it('returns true if all numbers in an array are odd and we test for odd numbers', () => {
-      // Your code goes here
+      const arr = [1, 3, 5, 7, 9];
+      expect(_.every(arr, item => item % 2 === 1)).toBe(true);
     });
 
     it('returns false if not all numbers in an array are odd and we test for odd numbers', () => {
-      // Your code goes here
+      const arr = [1, 3, 5, 6, 9];
+      expect(_.every(arr, item => item % 2 === 1)).toBe(false);
     });
 
   });
