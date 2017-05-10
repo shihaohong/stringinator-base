3fe4b5322d5cc477b419eb6635cacc8f094a0f5e
Shi-Hao Hong <shihaohong94@gmail.com>
2017-05-10 01:41:30 -0700

    added contains() functionality and Test Suite

--- test/underbar/contains.test.js
+++ test/underbar/contains.test.js
@@ -2,19 +2,33 @@ const _ = require('../../underbar');
 
 describe('contains()', () => {
   it('returns true if an array contains the target', () => {
-    // Your code goes here
+    const arr = [1, 3, 5, 7, 9];
+    expect(_.contains(arr, 3)).toBe(true);
   });
 
   it('returns false if an array does not contain the target', () => {
-    // Your code goes here
+    const arr = [1, 3, 5, 7, 9];
+    expect(_.contains(arr, 4)).toBe(false);
   });
 
   it('returns true if the target value is among the values of an object', () => {
-    // Your code goes here
+    const inventory = {
+      'Apple': 'MacBook',
+      'Asus': 'Zenbook Pro',
+      'HP': 'YogaBook'
+    }
+
+    expect(_.contains(inventory, 'YogaBook')).toBe(true);
   });
 
   it('returns false if the target value is not among the values of an object', () => {
-    // Your code goes here
+    const inventory = {
+      'Apple': 'MacBook',
+      'Asus': 'Zenbook Pro',
+      'HP': 'YogaBook'
+    }
+
+    expect(_.contains(inventory, 'TurtleBook')).toBe(false);
   });
 
 });
